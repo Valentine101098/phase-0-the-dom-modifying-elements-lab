5307788ec81e298af68d5c23db63d8d3d5cc2ac8
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-04-02 08:39:43 +0300

    created,added and removed elements from the DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'VAL-WANJIRU is the champion'
+document.body.append(newHeader);
+
+
+
